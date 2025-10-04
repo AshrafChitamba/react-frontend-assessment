e7c0a2c58da52a02dae97cacc5ecc27a1852dee4
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-10-04 16:05:04 +0200

    style - KnowledgeEntryCard

--- src/components/KnowledgeEntryCard.tsx
+++ src/components/KnowledgeEntryCard.tsx
@@ -14,21 +14,21 @@ export const KnowledgeEntryCard: FC<KnowledgeEntryCardProps> = ({
   onEdit,
 }) => {
   return (
-    <div className="border space-y-2 p-5 rounded-xl">
+    <div className="border space-y-3 py-8 px-7 rounded-xl bg-white">
       <h4 className="text-lg font-bold">{entry.title}</h4>
       <p className="text-[#627084] text-sm">{entry.description}</p>
 
-      <div className="grid grid-cols-2 item-center gap-3">
+      <div className="grid grid-cols-2 item-center gap-3 mt-5">
         <button
           onClick={() => onEdit(entry)}
-          className="border hover:border-blue-600 text-blue-600 hover:text-white hover:bg-blue-600/85 flex items-center justify-center gap-1.5 px-4 py-2 rounded-xl cursor-pointer"
+          className="border hover:border-blue-600 text-blue-600 hover:text-white bg-[#f9fafb] hover:bg-blue-600/85 flex items-center justify-center gap-1.5 px-4 py-2 rounded-xl cursor-pointer transition-colors"
         >
           <Edit size={14} />
           <span className="hidden sm:inline text-sm">Edit</span>
         </button>
         <button
           onClick={() => onDelete(entry.id)}
-          className="border hover:border-red-600 text-red-600 hover:text-white hover:bg-red-600/85 flex items-center justify-center gap-1.5 px-4 py-2 rounded-xl cursor-pointer"
+          className="border hover:border-red-600 text-red-600 hover:text-white bg-[#f9fafb] hover:bg-red-600/85 flex items-center justify-center gap-1.5 px-4 py-2 rounded-xl cursor-pointer transition-colors"
         >
           <Trash2 size={14} />
           <span className="hidden sm:inline text-sm">Delete</span>
