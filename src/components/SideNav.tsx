c45262b5115be541b99a0bd733d6d2613f361c85
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-10-05 10:10:08 +0200

    style - sidenav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,3 +1,17 @@
+import { Link } from "@tanstack/react-router";
+
 export const SideNav = () => {
-  return <aside>side nav</aside>;
+  return (
+    <aside className="border-r h-screen bg-white">
+      <div className="bg-black/90 text-white px-5 pt-6 pb-16">
+        <h1 className="text-4xl font-bold text-center uppercase">
+          Technician <br /> Portal
+        </h1>
+      </div>
+
+      <nav>
+        <Link to="/">Dashboard</Link>
+      </nav>
+    </aside>
+  );
 };
