485e994fa6e3882ce2c84f05df21ec1ea6475f2b
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-10-05 10:09:42 +0200

    style - home_route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,17 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Header } from "../components";
+import { SideNav } from "../components/SideNav";
 
 const RootLayout = () => (
-  <>
-    <Outlet />
-    <TanStackRouterDevtools />
-  </>
+  <main className="flex">
+    <SideNav />
+    <div className="flex-1">
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  </main>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { knowledgeEntries } from "../data";
+import { KnowledgeEntryCard } from "../components/KnowledgeEntryCard";
 
 export const KnowledgeEntriesPage = () => {
-  return <main>knowledge entries page</main>;
+  return (
+    <main>
+      <section className="container mx-auto px-4 py-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+          {knowledgeEntries.map((entry) => (
+            <KnowledgeEntryCard
+              entry={entry}
+              onDelete={() => {}}
+              onEdit={() => {}}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export const Route = createFileRoute("/")({
