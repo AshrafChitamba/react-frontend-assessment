eefca82f44aab3626b078c45ef17d4a4a3373008
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-10-05 16:56:22 +0200

    chore - settingup_tanstack_query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,13 +2,18 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Header } from "../components";
 import { SideNav } from "../components/SideNav";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => (
   <main className="flex flex-col md:flex-row gap-0">
     <SideNav />
     <div className="flex-1">
       <Header />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
       <TanStackRouterDevtools />
     </div>
   </main>
